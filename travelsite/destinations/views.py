f7e0b2f28e8ab57dc71bb5749868afa6c65bf853
Wolf Spirit <arilaen@gmail.com>
2013-12-08 02:07:03 -0500

    Corrected template paths

--- travelsite/destinations/views.py
+++ travelsite/destinations/views.py
@@ -32,7 +32,7 @@ def dest_form_view(request):
         dest_form = Dest_Form(request.POST, instance=a)
         if dest_form.is_valid():
             a = dest_form.save()
-            return HttpResponseRedirect('/destinations/dest_form_complete/')
+            return HttpResponseRedirect('destinations/dest_form_complete/')
             pass
     else:
         dest_form = Dest_Form(instance=a)
@@ -49,11 +49,11 @@ def review_form_view(request):
         review_form = Review_Form(request.POST, instance=a)
         if review_form.is_valid():
             a = review_form.save()
-            return HttpResponseRedirect('/destinations/dest_form_complete/')
+            return HttpResponseRedirect('destinations/dest_form_complete/')
             pass
     else:
         review_form = Review_Form(instance=a)
-    return render_to_response('/destinations/review_form.html', {
+    return render_to_response('destinations/review_form.html', {
         'review_form': review_form,
     }, context_instance=RequestContext(request))
 
@@ -66,11 +66,11 @@ def city_form_view(request):
         city_form = City_Form(request.POST, instance=a)
         if city_form.is_valid():
             city_form.save()
-            return HttpResponseRedirect('/destinations/dest_form_complete/')
+            return HttpResponseRedirect('destinations/dest_form_complete/')
             pass
     else:
         city_form = City_Form(instance=a)
-    return render_to_response('/destinations/city_form.html', {
+    return render_to_response('destinations/city_form.html', {
         'city_form': city_form,
     }, context_instance=RequestContext(request))
 
@@ -83,11 +83,11 @@ def country_form_view(request):
         country_form = Country_Form(request.POST, instance=a)
         if country_form.is_valid():
             country_form.save()
-            return HttpResponseRedirect('/destinations/dest_form_complete/')
+            return HttpResponseRedirect('destinations/dest_form_complete/')
             pass
     else:
         country_form = Country_Form(instance=a)
-    return render_to_response('/destinations/country_form.html', {
+    return render_to_response('destinations/country_form.html', {
         'country_form': country_form,
     }, context_instance=RequestContext(request))
 
@@ -107,7 +107,7 @@ def dest_edit_form_view(request):
     else:
         formset = FormSet(queryset=Destination.objects.all()
 )
-    return render_to_response('/destinations/dest_edit_form.html', {
+    return render_to_response('destinations/dest_edit_form.html', {
         'formset': formset,
     }, context_instance=RequestContext(request)) 
 
@@ -128,7 +128,7 @@ def review_form_edit_view(request):
            pass
    else:
        formset = FormSet
-   return render_to_response('/destinations/review_edit_form.html', {
+   return render_to_response('destinations/review_edit_form.html', {
        'formset': formset,
    }, context_instance=RequestContext(request))
 
@@ -146,7 +146,7 @@ def country_form_edit_view(request):
             pass
     else:
            formset = FormSet
-    return render_to_response('/destinations/country_edit_form.html', {
+    return render_to_response('destinations/country_edit_form.html', {
         'formset': formset,
     }, context_instance=RequestContext(request))
 
@@ -164,13 +164,13 @@ def city_form_edit_view(request):
             pass
     else:
            formset = FormSet()
-    return render_to_response('/destinations/city_edit_form.html', {
+    return render_to_response('destinations/city_edit_form.html', {
         'formset': formset,
     }, context_instance=RequestContext(request))
 
 def index(request):
     latest_destination_list = Destination.objects.all().order_by('-id')[:5]
-    t = loader.get_template('/destinations/index.html')
+    t = loader.get_template('destinations/index.html')
     c = RequestContext(request, {
         'latest_destination_list': latest_destination_list,
     })
@@ -183,7 +183,7 @@ def dest_detail(request, destination_id):
     city = p.city,
 #    q = get_object_or_404(City, pk=city.id)
  #   country = q.parent_country,
-    t = loader.get_template('/destinations/dest_detail.html')
+    t = loader.get_template('destinations/dest_detail.html')
 #    related_interests = p.interest.order_by('-id')
     c = Context({
         'name': name,
@@ -197,7 +197,7 @@ def dest_detail(request, destination_id):
 def country_detail(request, country_id):
     d = get_object_or_404(Country, pk=country_id)
     related_cities_list = d.cities
-    t = loader.get_template('/destinations/country_detail.html')
+    t = loader.get_template('destinations/country_detail.html')
     name = d.country_name
     c = Context ({
         'name': name,
@@ -211,7 +211,7 @@ def city_detail(request, city_id):
     destination_list = d.destination_set.all().order_by('-id')[:5]
     parent_country = d.parent_country
   #  related_interests = d.interest.order_by('-id')
-    t = loader.get_template('/destinations/city_detail.html')
+    t = loader.get_template('destinations/city_detail.html')
     c = Context ({
         'name': name,
         'destination_list': destination_list,
